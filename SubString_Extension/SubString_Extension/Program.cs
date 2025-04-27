f282c7ec54d85620dc2c29e808b5c87d24d3811f
Karen-xgit <ananyan2020>
2025-04-28 02:16:46 +0400

    add project SubString_Extension

---
+++ SubString_Extension/SubString_Extension/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SubString_Extension;
+static class Program
+{
+    public static string SubString(this string str, int start, int end)
+    {
+        if (start > end || start < 0 || end >= str.Length)
+        {
+            return "";
+        }
+
+        string newStr = "";
+        for (int i = start; i <= end; i++)
+        {
+            newStr += str[i];
+        }
+
+        return newStr;
+    }
+    static void Main(string[] args)
+    {
+        string str = "Hello world!";
+
+        Console.WriteLine(str.SubString(1, 1));
+    }
+}
